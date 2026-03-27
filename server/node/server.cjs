0808aefb1f37039b8ab72eaeea1c08bb8948facd
baek574 <baek574@gmail.com>
2026-03-28 01:01:41 +0900

    Update server.cjs

--- server/node/server.cjs
+++ server/node/server.cjs
@@ -10,9 +10,9 @@ const crypto = require('crypto')
 const rateLimit = require('express-rate-limit');
 const { WebSocketServer } = require('ws');
 app.use(express.static(path.join(process.cwd(), 'dist'), {index: false}));
-app.use(express.json({ limit: '100mb' }));
-app.use(express.raw({ type: 'application/octet-stream', limit: '100mb' }));
-app.use(express.text({ limit: '100mb' }));
+app.use(express.json({ limit: '200mb' }));
+app.use(express.raw({ type: 'application/octet-stream', limit: '200mb' }));
+app.use(express.text({ limit: '200mb' }));
 const {pipeline} = require('stream/promises')
 const https = require('https');
 const sslPath = path.join(process.cwd(), 'server/node/ssl/certificate');
